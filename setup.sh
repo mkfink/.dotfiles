ada69f252156873c132da12029739daf2f6dc1ee
mkfink <mkfink@gmail.com>
2020-06-03 10:55:32 -0400

    automate more things

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install git;
+fi
+cd ~
+git clone https://github.com/jimeh/tmux-themepack
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
